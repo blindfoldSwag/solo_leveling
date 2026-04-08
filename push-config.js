bef5b9bfa176e7936e4985ae82d74c7084578892
blindfoldSwag <105890645+blindfoldSwag@users.noreply.github.com>
2026-04-08 09:37:31 +0530

    Update push-config.js

--- push-config.js
+++ push-config.js
@@ -1,6 +1,6 @@
 window.SYSTEM_PUSH_CONFIG = {
   publicKey: 'BEsb24k-oCsQeYXat2pqOY2P_optqoC7J_ZExKfiOQLAeGJ6_LsJphVHYBDEnNVrBcVF7U-lw3ARF8O-k6kXTtI',
-  subscribeUrl: 'https://sl-system-notification-backend.onrender.com/api/notifications/subscribe',
-  stateSyncUrl: 'https://sl-system-notification-backend.onrender.com/api/notifications/state',
-  testUrl: 'https://sl-system-notification-backend.onrender.com/api/notifications/test'
+  subscribeUrl: 'https://sl-system-notifications.slsystem.workers.dev/api/notifications/subscribe',
+  stateSyncUrl: 'https://sl-system-notifications.slsystem.workers.dev/api/notifications/state',
+  testUrl: 'https://sl-system-notifications.slsystem.workers.dev/api/notifications/test'
 };
